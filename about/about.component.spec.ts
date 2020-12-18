5e815d4149a20166e515fcf1de5d00313c09ccbb
TepanD <75518831+TepanD@users.noreply.github.com>
2020-12-18 12:21:17 +0700

    Add files via upload

--- about/about.component.spec.ts
+++ about/about.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WeatherComponent } from './weather.component';
+import { AboutComponent } from './about.component';
 
-describe('WeatherComponent', () => {
-  let component: WeatherComponent;
-  let fixture: ComponentFixture<WeatherComponent>;
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WeatherComponent ]
+      declarations: [ AboutComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WeatherComponent);
+    fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- weather/weather.component.ts
+++ weather/weather.component.ts
@@ -1,3 +1,5 @@
+  
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -15,59 +17,79 @@ export class WeatherComponent implements OnInit {
   myDate6 = Date.now() + (24 * 60 * 60 * 1000)*5;
   myDate7 = Date.now() + (24 * 60 * 60 * 1000)*6;
 
+  myDate8 = Date.now() - (24 * 60 * 60 * 1000);
+  myDate9 = Date.now() - (24 * 60 * 60 * 1000)*2;
+  myDate10 = Date.now() - (24 * 60 * 60 * 1000)*3;
+  myDate11 = Date.now() - (24 * 60 * 60 * 1000)*4;
+
+  dateMinus1 = Math.round(this.myDate8 / 1000);
+  dateMinus2 = Math.round(this.myDate9 / 1000);
+  dateMinus3 = Math.round(this.myDate10 / 1000);
+  dateMinus4 = Math.round(this.myDate11 / 1000);
+
   WeatherData:any;
-  WeatherData2:any;
-  WeatherData3:any;
-  WeatherData4:any;
-  WeatherData5:any;
-  WeatherData6:any;
-  WeatherData7:any;
+  futureTemperature: any;
+  pastTemperature: any;
+  pastHumidity: any;
+  location: string;
+  longitude: number;
+  latitude: number;
+  isSearchSuccessful: boolean;
+
+  public hour: any;
+  public minute: string;
+  public second: string;
+  public ampm: string;
+
   constructor() { }
 
 
   ngOnInit(){
+    //console.log(this.dateMinus1);
+    //console.log(this.dateMinus2);
+    //console.log(this.dateMinus3);
+    //console.log(this.dateMinus4);
+    setInterval(() => {
+      const clock = new Date();
+      this.digitalclock(clock);
+    }, 1000);
+
     this.WeatherData = {
       main : {},
       isDay: true
     };
-    this.WeatherData2 = {
-      main : {},
-      isDay: true
-    };
-    this.WeatherData3 = {
-      main : {},
-      isDay: true
-    };
-    this.WeatherData4 = {
-      main : {},
-      isDay: true
-    };
-    this.WeatherData5 = {
-      main : {},
-      isDay: true
-    };
-    this.WeatherData6 = {
-      main : {},
-      isDay: true
-    };
-    this.WeatherData7 = {
-      main : {},
-      isDay: true
-    };
+    this.futureTemperature = [-1, -1, -1, -1, -1, -1];
+    this.pastTemperature = [-1, -1, -1, -1];
+    this.pastHumidity = [-1, -1, -1, -1];
+    this.latitude = 6.2088;
+    this.longitude = 106.8456;
+    //document.getElementById("submitLocation").addEventListener("click",
+    //function(event){
+    //  event.preventDefault();
+    //});
+    this.location = "Jakarta";
     this.getWeatherData();
-    console.log(this.WeatherData);
-    this.getWeatherData2();
-    console.log(this.WeatherData2);
-    this.getWeatherData3();
-    console.log(this.WeatherData3);
-    this.getWeatherData4();
-    console.log(this.WeatherData4);
-    this.getWeatherData5();
-    console.log(this.WeatherData5);
-    this.getWeatherData6();
-    console.log(this.WeatherData6);
-    this.getWeatherData7();
-    console.log(this.WeatherData7);
+    this.getPastWeatherData();
+    // console.log(this.WeatherData);
+    // console.log(this.myDate);
+  }
+
+  digitalclock(clock: Date){
+
+    const hours = clock.getHours();
+    
+    this.ampm = hours >= 12 ? 'PM' : 'AM';
+
+    this.hour = hours % 12;
+    this.hour = this.hour ? this.hour : 12;
+
+    this.hour = this.hour < 10 ? '0' + this.hour : this.hour;
+
+    const minutes = clock.getMinutes();
+    this.minute = minutes < 10 ? '0' + minutes : minutes.toString();
+
+    const seconds = clock.getSeconds();
+    this.second = seconds < 10 ? '0' + seconds : seconds.toString();
   }
 
   getWeatherData(){
@@ -75,156 +97,90 @@ export class WeatherComponent implements OnInit {
     //fetch('https://api.openweathermap.org/data/2.5/weather?q=jakarta&appid=75f224a9280b9d612de738569c117dbb')
     .then(response=>response.json())
     .then(data=>{this.setWeatherData(data);})
-    
   }
 
+  getWeatherDataFromForm(location: HTMLInputElement){
+    // alert(location.value);
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location.value + '&appid=75f224a9280b9d612de738569c117dbb')
+    .then(response=>response.json())
+    .then(data=>{this.getCoordinate(data);})
+  }
+
+  getCoordinate(data){
+    if (data.cod == 200){
+      this.latitude = data.coord.lat;
+      this.longitude = data.coord.lon;
+      this.location = data.name;
+      this.getWeatherData2();
+      this.getPastWeatherData();
+    }
+    else{
+      alert("Location Not Found");
+    }
+  }
 
   getWeatherData2(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=6.2088&lon=106.8456&dt=1608091200&appid=48c20bc83d40b87c5c4916f22048f261')
-    //fetch('https://api.openweathermap.org/data/2.5/weather?q=jakarta&appid=75f224a9280b9d612de738569c117dbb')
-    .then(response=>response.json())
-    .then(data2=>{this.setWeatherData2(data2);})
-    
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + this.latitude + '&lon=' + this.longitude + '&exclude=minutely,hourly,alerts&appid=75f224a9280b9d612de738569c117dbb')
+      .then(response=>response.json())
+      .then(data=>{this.setWeatherData(data);})
   }
 
-  getWeatherData3(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=6.2088&lon=106.8456&dt=1608177600&appid=eadbc9eb76bdc79b507598bc528dafa7')
-    //fetch('https://api.openweathermap.org/data/2.5/weather?q=jakarta&appid=75f224a9280b9d612de738569c117dbb')
-    .then(response=>response.json())
-    .then(data3=>{this.setWeatherData3(data3);})
-    
+  getPastWeatherData(){
+    this.getPastWeatherData1();
   }
 
-  getWeatherData4(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=6.2088&lon=106.8456&dt=1608264000&appid=365464cb4b4764eea0df8a2232678c5a')
-    //fetch('https://api.openweathermap.org/data/2.5/weather?q=jakarta&appid=75f224a9280b9d612de738569c117dbb')
+  getPastWeatherData1(){
+    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + this.latitude + '&lon=' + this.longitude + '&dt=' + this.dateMinus4 + '&appid=6781d6572766dc79429c5aee73ecf7e7')
     .then(response=>response.json())
-    .then(data4=>{this.setWeatherData4(data4);})
-    
+    .then(data=>{this.setPastWeatherData(data, 0);
+      this.getPastWeatherData2();
+    })
   }
 
-  getWeatherData5(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=6.2088&lon=106.8456&dt=1608350400&appid=2070571c07239e7d5497eb312394b3aa')
-    //fetch('https://api.openweathermap.org/data/2.5/weather?q=jakarta&appid=75f224a9280b9d612de738569c117dbb')
+  getPastWeatherData2(){
+    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + this.latitude + '&lon=' + this.longitude + '&dt=' + this.dateMinus3 + '&appid=6781d6572766dc79429c5aee73ecf7e7')
     .then(response=>response.json())
-    .then(data5=>{this.setWeatherData5(data5);})
-    
+    .then(data=>{this.setPastWeatherData(data, 1);
+      this.getPastWeatherData3();
+    })
   }
 
-  getWeatherData6(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=6.2088&lon=106.8456&dt=1608436800&appid=46ce7847e83df7187b5c9b38ce13ebbc')
-    //fetch('https://api.openweathermap.org/data/2.5/weather?q=jakarta&appid=75f224a9280b9d612de738569c117dbb')
+  getPastWeatherData3(){
+    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + this.latitude + '&lon=' + this.longitude + '&dt=' + this.dateMinus2 + '&appid=6781d6572766dc79429c5aee73ecf7e7')
     .then(response=>response.json())
-    .then(data6=>{this.setWeatherData6(data6);})
-    
+    .then(data=>{this.setPastWeatherData(data, 2);
+      this.getPastWeatherData4();
+    })
   }
 
-  getWeatherData7(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=6.2088&lon=106.8456&dt=1608523200&appid=59186581c68915b4109352c7f45ac07f')
-    //fetch('https://api.openweathermap.org/data/2.5/weather?q=jakarta&appid=75f224a9280b9d612de738569c117dbb')
+  getPastWeatherData4(){
+    fetch('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + this.latitude + '&lon=' + this.longitude + '&dt=' + this.dateMinus1 + '&appid=6781d6572766dc79429c5aee73ecf7e7')
     .then(response=>response.json())
-    .then(data7=>{this.setWeatherData7(data7);})
-    
+    .then(data=>{this.setPastWeatherData(data, 3);})
   }
+
   setWeatherData(data: any){
     this.WeatherData = data;
     //let sunsetTime = new Date(this.WeatherData.current.sunset * 1000);
     //this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
     //let currentDate = new Date();
     //this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.current.temp - 273.15).toFixed(0);
+    this.WeatherData.temp_celcius = (this.WeatherData.current.temp - 273.15).toFixed(1);
+    this.futureTemperature[0] = (this.WeatherData.daily[0].temp.day - 273.15).toFixed(1);
+    this.futureTemperature[1] = (this.WeatherData.daily[1].temp.day - 273.15).toFixed(1);
+    this.futureTemperature[2] = (this.WeatherData.daily[2].temp.day - 273.15).toFixed(1);
+    this.futureTemperature[3] = (this.WeatherData.daily[3].temp.day - 273.15).toFixed(1);
+    this.futureTemperature[4] = (this.WeatherData.daily[4].temp.day - 273.15).toFixed(1);
+    this.futureTemperature[5] = (this.WeatherData.daily[5].temp.day - 273.15).toFixed(1);
     //this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
     //this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
     //this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    this.WeatherData.wind_speed = (this.WeatherData.current.wind_speed).toFixed(0);
+    this.WeatherData.current.wind_speed = (this.WeatherData.current.wind_speed).toFixed(0);
+  }
 
-
-
+  setPastWeatherData(data: any, index: number){
+    this.pastTemperature[index] = (data.current.temp - 273.15).toFixed(1);
+    this.pastHumidity[index] = data.current.humidity;
   }
-  setWeatherData2(data2: any){
-    this.WeatherData2 = data2;
-    /*//let sunsetTime = new Date(this.WeatherData.current.sunset * 1000);
-    //this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
-    //let currentDate = new Date();
-    //this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.current.temp - 273.15).toFixed(0);
-    //this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    //this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    //this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    this.WeatherData.wind_speed = (this.WeatherData.current.wind_speed).toFixed(0);*/
-
-    this.WeatherData2.temp_celcius = (this.WeatherData2.current.temp - 273.15).toFixed(0);
-    this.WeatherData2.wind_speed = (this.WeatherData2.current.wind_speed).toFixed(0);
-  }
-  setWeatherData3(data3: any){
-    this.WeatherData3 = data3;
-    /*//let sunsetTime = new Date(this.WeatherData.current.sunset * 1000);
-    //this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
-    //let currentDate = new Date();
-    //this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.current.temp - 273.15).toFixed(0);
-    //this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    //this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    //this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    this.WeatherData.wind_speed = (this.WeatherData.current.wind_speed).toFixed(0);*/
-    this.WeatherData3.temp_celcius3 = (this.WeatherData3.current.temp - 273.15).toFixed(0);
-    this.WeatherData3.wind_speed = (this.WeatherData3.current.wind_speed).toFixed(0);
-  }
-  setWeatherData4(data4: any){
-    this.WeatherData4 = data4;
-    /*//let sunsetTime = new Date(this.WeatherData.current.sunset * 1000);
-    //this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
-    //let currentDate = new Date();
-    //this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.current.temp - 273.15).toFixed(0);
-    //this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    //this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    //this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    this.WeatherData.wind_speed = (this.WeatherData.current.wind_speed).toFixed(0);*/
-    this.WeatherData4.temp_celcius = (this.WeatherData4.current.temp - 273.15).toFixed(0);
-    this.WeatherData4.wind_speed = (this.WeatherData4.current.wind_speed).toFixed(0);
-  }
-  setWeatherData5(data5: any){
-    this.WeatherData5 = data5;
-    /*//let sunsetTime = new Date(this.WeatherData.current.sunset * 1000);
-    //this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
-    //let currentDate = new Date();
-    //this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.current.temp - 273.15).toFixed(0);
-    //this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    //this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    //this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    this.WeatherData.wind_speed = (this.WeatherData.current.wind_speed).toFixed(0);*/
-    this.WeatherData5.temp_celcius = (this.WeatherData5.current.temp - 273.15).toFixed(0);
-    this.WeatherData5.wind_speed = (this.WeatherData5.current.wind_speed).toFixed(0);
-  }
-  setWeatherData6(data6: any){
-    this.WeatherData6 = data6;
-    /*//let sunsetTime = new Date(this.WeatherData.current.sunset * 1000);
-    //this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
-    //let currentDate = new Date();
-    //this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.current.temp - 273.15).toFixed(0);
-    //this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    //this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    //this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    this.WeatherData.wind_speed = (this.WeatherData.current.wind_speed).toFixed(0);*/
-    this.WeatherData6.temp_celcius = (this.WeatherData6.current.temp - 273.15).toFixed(0);
-    this.WeatherData6.wind_speed = (this.WeatherData6.current.wind_speed).toFixed(0);
-  }
-  setWeatherData7(data7: any){
-    this.WeatherData7 = data7;
-    /*//let sunsetTime = new Date(this.WeatherData.current.sunset * 1000);
-    //this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
-    //let currentDate = new Date();
-    //this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.current.temp - 273.15).toFixed(0);
-    //this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    //this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    //this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    this.WeatherData.wind_speed = (this.WeatherData.current.wind_speed).toFixed(0);*/
-    this.WeatherData7.temp_celcius = (this.WeatherData7.current.temp - 273.15).toFixed(0);
-    this.WeatherData7.wind_speed = (this.WeatherData7.current.wind_speed).toFixed(0);
-    
-  }
+  
 }
